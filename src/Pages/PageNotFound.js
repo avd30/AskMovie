d2ca8dc56e0d109cac32c0313a8a34539d51caf0
avd30 <aviral2002@gmail.com>
2023-06-07 07:46:19 +0530

    environment var updated

--- src/Pages/PageNotFound.js
+++ src/Pages/PageNotFound.js
@@ -3,7 +3,7 @@
 export const PageNotFound = () => {
   return (
     <main>
-      <h1 className='text-3xl text-gray-700 dark:text-white'>PageNotFound</h1>
+      <h1 className='text-3xl text-gray-700 dark:text-white'>Page Not Found</h1>
     </main>
   )
 }
